c35868fc65d47368c976c6502cf257bd1b4b39e2
ajreddy425 <ajreddy1096@gmail.com>
2025-12-04 20:34:27 +0530

    updated

--- cidr_subnets.tf
+++ cidr_subnets.tf
@@ -1,14 +1,14 @@
-# resource "aws_subnet" "sub1" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index) // dynamically create subnet cidr from vpc cidr
-#   availability_zone = local.l_azs[count.index]
-#   count             = local.c_azs
+resource "aws_subnet" "sub1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index) // dynamically create subnet cidr from vpc cidr
+  availability_zone = local.l_azs[count.index]
+  count             = local.c_azs
 
-#   tags = {
-#     Name = "pub_subnet-${count.index + 1}-${local.ws}"
-#   }
+  tags = {
+    Name = "pub_subnet-${count.index + 1}-${local.ws}"
+  }
 
-# }
+}
 
 # resource "aws_subnet" "sub2" {
 #   vpc_id            = aws_vpc.main.id
--- ec2_pub1.tf
+++ ec2_pub1.tf
@@ -2,13 +2,35 @@
 #   ami                         = "ami-0fa3fe0fa7920f68e"
 #   instance_type               = "t2.micro"
 #   associate_public_ip_address = true
-#   count                       = 2
+#   count                       = 1
 #   key_name                    = "terraform-1711"
 #   vpc_security_group_ids      = [aws_security_group.all_sg.id]
 #   subnet_id                   = element(aws_subnet.sub1.*.id, count.index)
 #   availability_zone           = local.l_azs[count.index]
 #   depends_on                  = [aws_security_group.all_sg]
 #   user_data                   = file("./scripts/apache1.sh")
+
+#   provisioner "local-exec" {
+#     command = "echo ${aws_instance.web1.*.public_ip[count.index]} >> ip_web1.txt"
+    
+#   }
+
+# provisioner "remote-exec" {
+#   inline = [
+#     "sudo yum update -y",
+#     "sudo yum install -y httpd",
+#     "sudo service start httpd",
+#     "sudo systemctl enable httpd",
+#     "echo '<h1>Welcome to Web Server 1 - ${terraform.workspace}</h1>' | sudo tee /var/www/html/index.html"
+#   ]
+
+#   connection {
+#     type        = "ssh"
+#     user        = "ec2-user"
+#     private_key = file("~/.ssh/terraform-1711.pem")
+#     host        = aws_instance.web1[count.index].public_ip
+#   }
+# }
 
 #   tags = {
 #     Name = "ec2-web1-${count.index + 1}-${terraform.workspace}"
